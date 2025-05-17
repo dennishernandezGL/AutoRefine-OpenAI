0d5ba0df90e335fafd56ced2dfc58fdabd03257a
Dennis Hernandez <v-dhernandez@youngliving.com>
2025-05-17 09:08:43 -0600

    Add OpenAILambda project to solution and update InfraStack configuration

--- Infra/InfraStack.cs
+++ Infra/InfraStack.cs
@@ -7,11 +7,11 @@ public class InfraStack : Stack
 {
     internal InfraStack(App scope, string id, StackProps? props = null) : base(scope, id, props)
     {
-        var fn = new Function(this, "MyDotnetFunction", new FunctionProps
+        var fn = new Function(this, "OpenAILambda", new FunctionProps
         {
             Runtime = Runtime.DOTNET_6,
-            Handler = "AutoRefineOpenAI::MyLambda.Function::FunctionHandler",
-            Code = Code.FromAsset(Path.Combine("..","lambda","MyLambda","bin","Release","net6.0","publish"))
+            Handler = "OpenAILambda::OpenAILambda.Function::FunctionHandler",
+            Code = Code.FromAsset(Path.Combine("..","OpenAILambda","bin","Release","net6.0","publish"))
         });
     }
 }
