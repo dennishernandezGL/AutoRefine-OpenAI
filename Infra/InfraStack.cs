4c72adb9e8d6be85dcc2a75c63424d68997f79be
Dennis Hernandez <v-dhernandez@youngliving.com>
2025-05-17 11:01:32 -0600

    Add EventBridge rule to schedule Lambda function invocation and output details

--- Infra/InfraStack.cs
+++ Infra/InfraStack.cs
@@ -1,4 +1,6 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.Events;
+using Amazon.CDK.AWS.Events.Targets;
 using Amazon.CDK.AWS.Lambda;
 
 namespace Infra;
@@ -8,9 +10,6 @@ public class InfraStack : Stack
     internal InfraStack(App scope, string id, StackProps? props = null) : base(scope, id, props)
     {
         var lambdaCodePath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "OpenAILambda", "bin", "Release", "net6.0", "publish"));
-        
-        Console.WriteLine($"Lambda code path: {lambdaCodePath}");
-        Console.WriteLine($"Directory exists: {Directory.Exists(lambdaCodePath)}");
         
         if (!Directory.Exists(lambdaCodePath))
         {
@@ -26,10 +25,31 @@ public class InfraStack : Stack
             MemorySize = 256
         });
         
+        var scheduleTime = System.Environment.GetEnvironmentVariable("SCHEDULE_TIME") ?? "5";
+        var rule = new Rule(this, "ScheduleRule", new RuleProps
+        {
+            Schedule = Schedule.Rate(Duration.Minutes(Convert.ToDouble(scheduleTime))),
+            Description = "Schedule for invoking OpenAI Lambda function every X minutes"
+        });
+        
+        rule.AddTarget(new LambdaFunction(fn));
+        
+        new CfnOutput(this, "LogGroupUrl", new CfnOutputProps
+        {
+            Value = $"https://console.aws.amazon.com/cloudwatch/home?region={this.Region}#logsV2:log-groups/log-group/{fn.LogGroup.LogGroupName}",
+            Description = "URL to CloudWatch Logs for the Lambda function"
+        });
+        
         new CfnOutput(this, "LambdaFunctionArn", new CfnOutputProps
         {
             Value = fn.FunctionArn,
             Description = "ARN of the Lambda function"
+        });
+        
+        new CfnOutput(this, "ScheduleRuleName", new CfnOutputProps
+        {
+            Value = rule.RuleName,
+            Description = "Name of the EventBridge rule that schedules the Lambda"
         });
     }
 }
