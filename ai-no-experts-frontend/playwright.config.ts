e42c2102bc4e0e9a0e39173f83d9e41623ec50c2
luis.corrales <luis.corrales@lcodecr.com>
2025-05-17 14:58:02 -0600

    fix build

--- ai-no-experts-frontend/playwright.config.ts
+++ ai-no-experts-frontend/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   use: {
-    baseURL: 'http://localhost:5175/',
+    baseURL: ' http://localhost:5177/',
     headless: false, // ponlo en true en CI
     viewport: { width: 1280, height: 720 },
     screenshot: 'on',
