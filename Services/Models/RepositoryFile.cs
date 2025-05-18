66942fff09469824534c1cc6a672c64ddb439567
Dennis Hernandez <v-dhernandez@youngliving.com>
2025-05-17 18:55:41 -0600

    feat: configure Swagger for API documentation and add necessary service dependencies

---
+++ Services/Models/RepositoryFile.cs
@@ -0,0 +1,9 @@
+namespace Services.Models;
+
+
+public class RepositoryFile
+{
+    public string Path { get; set; }
+    public string Name { get; set; }
+    public string Content { get; set; }
+}
